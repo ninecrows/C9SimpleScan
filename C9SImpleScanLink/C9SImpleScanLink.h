cf6cd5b1dd6dc87983bedc534364ec68fc28bfc1
Kyle Wilson <kyle@ninecrows.com>
2018-02-05 20:42:52 -0500

    Initial commit to preserve stock project files

---
+++ C9SImpleScanLink/C9SImpleScanLink.h
@@ -0,0 +1,13 @@
+// C9SImpleScanLink.h
+
+#pragma once
+
+using namespace System;
+
+namespace C9SImpleScanLink {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
